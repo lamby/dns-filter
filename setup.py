84426956101a3a37334177d11843533a87032da7
Chris Lamb <chris@chris-lamb.co.uk>
2015-11-05 18:29:31 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='dns-filter',
+
+    url='https://chris-lamb.co.uk/projects/dns-filter',
+    version='0.0.1',
+    description="A simple DNS proxy that returns NXDOMAIN if the master "
+        "offers an IP from a specified blacklist.",
+
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+
+    scripts=(
+        'dns-filter',
+    ),
+)
